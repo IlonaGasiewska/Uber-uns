07671ce129779fed8c61f51182f6c23f450d0d7c
Ilona G <djkojan@o2.pl>
2023-05-14 12:05:44 +0200

    forEach loop

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ const img = document.querySelectorAll(".carousel-photo");
 const popup = document.querySelector(".popup");
 const popupPhoto = document.querySelector(".popup-photo");
 
-for (i=0; i<=(img.length - 1); i++)
-    {
-        img[i].addEventListener("click", (event)=>{ popup.style.display="flex";
+img.forEach((image)=>{
+        image.addEventListener("click", (event)=>{ popup.style.display="flex";
         popupPhoto.setAttribute("src", event.currentTarget.getAttribute("src"));
         popup.addEventListener("click", ()=>{popup.style.display="none"});
         });
-    } 
+    });
 
 
 
